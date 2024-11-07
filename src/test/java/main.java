98280c0b8dc43968742228e88f7e851f4c88f130
shoxizuz <infotrustedrepair@gmail.com>
2024-11-06 22:26:45 -0500

    first commit

---
+++ src/test/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("this is the project version 1");
+
+    }
+}
+// done with version 1
+
