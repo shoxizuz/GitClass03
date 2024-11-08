0692a9f3ab3cb3a4d00bd77a9d339a92cbbdbb62
shoxizuz <infotrustedrepair@gmail.com>
2024-11-07 22:04:20 -0500

    third commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,11 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("this is the project version 2");
-        System.out.println("this is the project");
-        System.out.println("this is the");
+        System.out.println("this is the project version 3");
 
     }
 }
-// done with version 2
+// done with version 3
 
