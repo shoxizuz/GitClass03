427cccdcc081799e6aedc97f47fa1471fa3874db
shoxizuz <infotrustedrepair@gmail.com>
2024-11-07 21:59:20 -0500

    second commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,9 +1,11 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("this is the project version 1");
+        System.out.println("this is the project version 2");
+        System.out.println("this is the project");
+        System.out.println("this is the");
 
     }
 }
-// done with version 1
+// done with version 2
 
